7b52c1e9c5f74dd45fa549f9dc1ddd525bd0823a
zym-bass <824122161@qq.com>
2020-12-16 08:56:24 +0800

    使用try with resource释放SqlSession资源

--- MyBatis-course/mybatis_ch01/src/test/java/com/yiming/MyBatis_Test.java
+++ MyBatis-course/mybatis_ch01/src/test/java/com/yiming/MyBatis_Test.java
@@ -19,19 +19,25 @@ public class MyBatis_Test {
     @Test
     //修改Person
     public void updatePerson(){
+        SqlSession sqlSession =null;
         try {
-           SqlSession sqlSession =new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("sqlMapperConfig.xml")).openSession();
+           sqlSession =new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("sqlMapperConfig.xml")).openSession();
            sqlSession.update("com.yiming.entity.PersonMapper.updatePerson", new Person(1, "大一名", "男", new Date(), 20));
            sqlSession.commit();
-           sqlSession.close();
+
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if(sqlSession!=null){
+                sqlSession.close();
+            }
         }
     }
 
     @Test
     //根据Id删除Person
     public  void deletePerson(){
+        SqlSession sqlSession =null;
         try {
             //1. 定义主配置文件，路径从主目标文件开始写（target/classes）
             String config = "sqlMapperConfig.xml";
@@ -40,7 +46,7 @@ public class MyBatis_Test {
             //3. 通过SqlSessionFactoryBuilder 获取SqlSessionFactory
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
             //4. 通过SqlSessionFactory 获取到SqlSession
-            SqlSession sqlSession = factory.openSession();
+            sqlSession = factory.openSession();
             //5. SqlSession方法执行sql语句
             System.out.println(sqlSession.delete("com.yiming.entity.PersonMapper.deletePerson", 1));
             //6. 通过SqlSession提交事务
@@ -49,12 +55,17 @@ public class MyBatis_Test {
             sqlSession.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if (sqlSession!=null){
+                sqlSession.close();
+            }
         }
     }
 
     //通过id查询Person
     @Test
     public void selectIdPerson(){
+        SqlSession sqlSession =null;
         try {
             //1. 定义sqlMapperConfig主配置文件.从类路径的根开始写，(target/classes)
             String config = "sqlMapperConfig.xml";
@@ -64,7 +75,7 @@ public class MyBatis_Test {
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
 
             //4. 通过SqlSessionFactory获取SqlSession
-            SqlSession sqlSession = factory.openSession();
+            sqlSession = factory.openSession();
 
             //5. 确定指定的sql映射文件和指定sql语句 命名空间.id
             String id = "com.yiming.entity.PersonMapper.selectPerson";
@@ -74,6 +85,10 @@ public class MyBatis_Test {
             System.out.println(person);
         } catch (IOException e) {
             e.printStackTrace();
+        }finally{
+            if (sqlSession!=null) {
+                sqlSession.close();
+            }
         }
 
     }
@@ -81,6 +96,7 @@ public class MyBatis_Test {
     //插入Person数据
     @Test
     public void insertPerson() {
+        SqlSession sqlSession =null;
         try {
             //1. 定义mybatis主配置文件的名称,从类路径的根开始（target/classes）
             String config = "sqlMapperConfig.xml";
@@ -92,7 +108,7 @@ public class MyBatis_Test {
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
 
             //4. 获取SqlSession对象，从SqlSessionFactory获取
-            SqlSession sqlSession = factory.openSession();
+            sqlSession = factory.openSession();
 
             //5. 指定要执行的sql语句的标识
             //sql映射文件中的namespace.标签中的id值
@@ -108,10 +124,14 @@ public class MyBatis_Test {
             sqlSession.commit();
 
 
-            //8. 关闭session
-            sqlSession.close();
+
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if (sqlSession!=null){
+                //8. 关闭session
+                sqlSession.close();
+            }
         }
     }
 
@@ -122,15 +142,20 @@ public class MyBatis_Test {
      */
     @Test
     public void selectAll(){
+
         try {
             SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("sqlMapperConfig.xml"));
-            SqlSession sqlSession = factory.openSession();
-            PersonMapper mapper = sqlSession.getMapper(PersonMapper.class);
-            List<Person> list = mapper.selectAll();
-            list.forEach((Person s)->{
-                System.out.println(s);
-            });
 
+            try(
+                    SqlSession sqlSession = factory.openSession()
+            ){
+
+                PersonMapper mapper = sqlSession.getMapper(PersonMapper.class);
+                List<Person> list = mapper.selectAll();
+                list.forEach((Person s)->{
+                    System.out.println(s);
+                });
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -138,11 +163,13 @@ public class MyBatis_Test {
 
     @Test
     public void addPerson(){
-        SqlSession session = MybatisUtils.creatSqlSession();
-        PersonMapper mapper = session.getMapper(PersonMapper.class);
-        mapper.addPerson(new Person("李洋", "女", new Date(), 22));
-        session.commit();
-        session.close();
+        try (
+                SqlSession session = MybatisUtils.creatSqlSession()
+        ){
 
+            PersonMapper mapper = session.getMapper(PersonMapper.class);
+            mapper.addPerson(new Person("李洋", "女", new Date(), 22));
+            session.commit();
+        }
     }
 }

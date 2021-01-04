8bf5c1a926cbf46e3fb7223e533d3d4fb762c5d5
zym-bass <824122161@qq.com>
2021-01-04 19:21:26 +0800

    使用Jedis操作Redis--查询数据

--- Spring-course/Redis-Jedis/src/test/java/RedisTest.java
+++ Spring-course/Redis-Jedis/src/test/java/RedisTest.java
@@ -10,4 +10,15 @@ public class RedisTest {
         jedis.close();
 
     }
+
+
+
+    @Test
+    public void testSelectString(){
+        Jedis jedis = new Jedis("192.168.230.201",6379);
+        String name = jedis.get("name");
+        System.out.println(name);
+        jedis.close();
+
+    }
 }
